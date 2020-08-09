3f3c51468f4269b0fab7f13ca32958d1ad3a4954
Nancy Jackson <nbjackson23@gmail.com>
2020-08-09 17:08:10 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  def bark  
+    puts 'Woof!'
+  
+end
+
+fido = Dog.new 
+fido.bark
+
+  def sit
+    puts "The Dog is sitting"
+  
+end
+
+fido = Dog.new
+fido.sit
---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+  def talk
+    puts "Hello World!"
+  
+end
+
+alan_kay = Person.new
+alan_kay.talk
+
+  def walk
+    puts "The Person is walking"
+
+end
+
+alan_kay = Person.new
+alan_kay.walk
