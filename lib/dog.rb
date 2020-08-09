c8f59b787a6dbd9a151af37272af75512fa2448f
Nancy Jackson <nbjackson23@gmail.com>
2020-08-09 17:26:43 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class Dog
+  def bark   # <=== both of these methods are inside the class
+    puts 'Woof!'
+  end
+  
+   def sit  # <=== both of these methods are inside the class
+    puts "The Dog is sitting"
+  end
+end
+
---
+++ lib/person.rb
@@ -0,0 +1,9 @@
+class Person
+  def talk  # <=== both of these methods are inside the class
+    puts 'Hello World!'
+  end
+  
+   def walk  # <=== both of these methods are inside the class
+    puts "The Person is walking"
+  end
+end
